a0341de4c2c87d43f73b935dc0400480ea46826d
Your NameChristian <christianhermanndev@gmail.com>
2026-01-30 14:29:51 -0600

    Intro to Node

---
+++ introduction.js
@@ -0,0 +1,18 @@
+// Example of using template literals with strings and then running code in Node
+let noun1 = "Kit";
+let adjective = "silly";
+let noun2 = "dog";
+let verb = "funny";
+let noun3 = "house";
+
+console.log(
+  `The world's first ${noun1} was a very ${adjective} ${noun2} who loved to ${verb} while eating ${noun3} for every meal.`
+);
+
+// run in node
+
+/*
+$ node app.js
+The world's's first Kit was a very silly dog who loved to funny while eating house for every meal.
+$ 
+*/
