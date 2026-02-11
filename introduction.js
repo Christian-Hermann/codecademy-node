d74a9fe54d5d78fe79735cb4de455d013587e820
Your NameChristian <christianhermanndev@gmail.com>
2026-02-11 14:27:15 -0600

    Introduction review

--- introduction.js
+++ introduction.js
@@ -64,3 +64,56 @@ console.log(
 );
 
 // THE OS MODULE
+const os = require("os");
+
+const server = {
+  type: os.type(),
+  architecture: os.arch(),
+  uptime: os.uptime(),
+};
+console.log(server);
+
+// THE UTIL MODULE
+// Require in trails module from trails.js
+const trails = require("./trails.js");
+// Require in util module here
+const util = require("util");
+
+// Simulate database call to search trails module for specified trail
+const getTrailDistance = (trail, callback) => {
+  return setTimeout(() => {
+    if (trails.hasOwnProperty(trail)) {
+      const foundTrail = trails[trail];
+      callback(null, foundTrail);
+    } else {
+      callback(new Error("Trail not found!"));
+    }
+  }, 250); // short timeout needed for testing
+};
+
+// Callback function to send an error in the case of an error, or to handle trail data if a trail was found successfully.
+function callback(error, trailData) {
+  if (error) {
+    console.error(error.message);
+    process.exit(1);
+  } else {
+    const mi = trailData.miles;
+    const nickname = trailData.nickname;
+    console.log(`The ${nickname} is ${mi} miles long!`);
+  }
+}
+
+getTrailDistance("North Country", callback);
+
+// Promisify below!
+const getTrailDistancePromise = util.promisify(getTrailDistance);
+
+async function tryGetTrailDistance(trailName) {
+  try {
+    const trail = await getTrailDistancePromise(trailName);
+    console.log(`The ${trail.nickname} is ${trail.miles} miles long!
+`);
+  } catch (e) {
+    console.log(`Trail not found ${trailName}`);
+  }
+}
