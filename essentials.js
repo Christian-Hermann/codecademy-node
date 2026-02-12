9b01e97cff27be36b92ad1eb65a01cfa025642cd
Your NameChristian <christianhermanndev@gmail.com>
2026-02-12 15:01:58 -0600

    Event listeners

---
+++ essentials.js
@@ -0,0 +1,13 @@
+// THE EVENTS MODULE
+// Here we require in the 'events' module and save a reference to it in an events variable
+let events = require("events");
+
+let listenerCallback = (data) => {
+  console.log("Celebrate " + data);
+};
+
+let myEmitter = new events.EventEmitter();
+
+myEmitter.on("celebration", listenerCallback);
+
+myEmitter.emit("celebration", "party-time");
