9ace51851502b5ef4404f1a96650f33f3b996c96
Your NameChristian <christianhermanndev@gmail.com>
2026-02-16 11:12:46 -0600

    User Input/Output

--- essentials.js
+++ essentials.js
@@ -11,3 +11,15 @@ let myEmitter = new events.EventEmitter();
 myEmitter.on("celebration", listenerCallback);
 
 myEmitter.emit("celebration", "party-time");
+
+// USER INPUT/OUTPUT
+let { testNumber } = require("./game.js");
+
+process.stdout.write(
+  'I\'m thinking of a number from 1 through 10. What do you think it is? \n(Write "quit" to give up.)\n\nIs the number ... '
+);
+
+let playGame = (userInput) => {
+  let input = userInput.toString().trim();
+  testNumber(input);
+};
