a36abb8a4449fb7e813df7cd0c8c51ebb7dcbaa8
Your NameChristian <christianhermanndev@gmail.com>
2026-02-20 13:49:44 -0600

    Errors in Node

--- essentials.js
+++ essentials.js
@@ -23,3 +23,17 @@ let playGame = (userInput) => {
   let input = userInput.toString().trim();
   testNumber(input);
 };
+
+// ERRORS
+const api = require("./api.js");
+
+// An error-first callback
+let errorFirstCallback = (err, data) => {
+  if (err) {
+    console.log(`Something went wrong. ${err}\n`);
+  } else {
+    console.log(`Something went right. Data: ${data}\n`);
+  }
+};
+
+api.errorProneAsyncApi("problematic input", errorFirstCallback);
