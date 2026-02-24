0104c9fba3c0a1b9afa78574b3cd6437d7eed66d
Your NameChristian <christianhermanndev@gmail.com>
2026-02-24 14:50:42 -0600

    The URL model

--- settingUpAServer.js
+++ settingUpAServer.js
@@ -9,3 +9,15 @@ server.listen(8080, () => {
   const { address, port } = server.address();
   console.log(`Server is listening on: http://${address}:${port}`);
 });
+
+// THE URL MODULE
+const URL_TO_PARSE =
+  "https://www.example.com/p/a/t/h?prop1=value1&prop2=value2";
+
+const myUrl = new URL(
+  "https://www.example.com/p/a/t/h?prop1=value1&prop2=value2"
+);
+
+const hostname = myUrl.hostname;
+const pathname = myUrl.pathname;
+const searchParams = myUrl.searchParams;
