277dda9beb192a150cb7008f38c7e80a2483457b
Your NameChristian <christianhermanndev@gmail.com>
2026-02-25 12:07:59 -0600

    The querystring module

--- settingUpAServer.js
+++ settingUpAServer.js
@@ -21,3 +21,19 @@ const myUrl = new URL(
 const hostname = myUrl.hostname;
 const pathname = myUrl.pathname;
 const searchParams = myUrl.searchParams;
+
+// The Querystring Module
+const querystring = require("querystring");
+
+const url = "https://www.example.com/p/a/t/h?course=node&lesson=http";
+
+// Isolate query string from url
+const queryToParse = url.split("?")[1];
+console.log(queryToParse);
+
+// Parse query string into object, then add new property
+const parsedQuery = querystring.parse(queryToParse);
+parsedQuery.exercise = "querystring";
+
+// Rebuild query string from object
+const modifiedQueryString = querystring.stringify(parsedQuery);
