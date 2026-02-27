085990ddd183ad2d6854c200699bea94d52e9a2e
Your NameChristian <christianhermanndev@gmail.com>
2026-02-27 13:44:07 -0600

    Rock, paper, scissors exercise setup

--- RESTful.js
+++ RESTful.js
@@ -1,3 +1,4 @@
+/*
 There are 4 basic HTTP verbs we use in requests to interact with resources in a REST system:
 
 GET — retrieve a specific resource (by id) or a collection of resources
@@ -5,3 +6,4 @@ POST — create a new resource
 PUT — update a specific resource (by id)
 DELETE — remove a specific resource by id
 You can learn more about these HTTP verbs in the following Codecademy article:
+*/
